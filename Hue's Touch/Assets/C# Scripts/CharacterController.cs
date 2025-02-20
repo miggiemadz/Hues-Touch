41e329b9b4bc658f9b478bde5b5084ca5af23e9d
Miggie <109484716+miggiemadz@users.noreply.github.com>
2025-02-20 13:29:35 -0500

    Player Prefab, Input Folder

--- Hue's Touch/Assets/C# Scripts/CharacterController.cs
+++ Hue's Touch/Assets/C# Scripts/CharacterController.cs
@@ -4,16 +4,15 @@ public class NewMonoBehaviourScript : MonoBehaviour
 {
     [SerializeField] private float playerMass;
 
-    [SerializeField] private Transform playerPosition;
     [SerializeField] private Transform feetPosition;
     [SerializeField] private Rigidbody rb;
+    
 
     private Vector3 gravityVector;
-    private Vector3 groundCheckVector = new Vector3(0,-.5f, 0);
+    private RaycastHit groundCheck;
 
     private void Awake()
     {
-        playerPosition = gameObject.GetComponent<Transform>();
         rb = gameObject.GetComponent<Rigidbody>();
         gravityVector = new Vector3(0, -9.8f * playerMass, 0);
         feetPosition = transform.GetChild(0).GetComponent<Transform>();
@@ -35,11 +34,14 @@ public class NewMonoBehaviourScript : MonoBehaviour
         {
             rb.MovePosition(transform.position + gravityVector * Time.fixedDeltaTime);
         }
+
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward), Color.yellow);
     }
 
     private bool isGrounded()
     {
-        RaycastHit hit;
-        return Physics.Raycast(feetPosition.position, transform.TransformDirection(Vector3.down), out hit, .2f);
+        Physics.Raycast(feetPosition.position, transform.TransformDirection(Vector3.down), out groundCheck, Mathf.Infinity);
+        Debug.DrawRay(feetPosition.position, transform.TransformDirection(Vector3.down * groundCheck.distance), Color.green);
+        return groundCheck.distance < .2f;
     }
 }
