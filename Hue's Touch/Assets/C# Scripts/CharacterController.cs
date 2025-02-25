c162acc75bffe4d84fc6adb7e4e760a4349079d3
Miggie <miguelt613377@gmail.com>
2025-02-25 16:05:12 -0500

    Grounded Controller Finished + Cinemachine

--- Hue's Touch/Assets/C# Scripts/CharacterController.cs
+++ Hue's Touch/Assets/C# Scripts/CharacterController.cs
@@ -5,31 +5,32 @@ using UnityEngine.InputSystem;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    [SerializeField] private float playerMass;
-    [SerializeField] private float playerRotateSpeed;
-    [SerializeField] private float playerGroundMoveAcceleration;
-    [SerializeField] private float playerGroundMoveDecceleration;
-    [SerializeField] private float PLAYER_MAX_SPEED;
-    private Vector2 playerGroundMoveSpeed;
-    private Vector2 playerGroundMoveVelocity;
-    private Quaternion playerDirection;
+    [Header("Grounded Movement Variables")]
+    [SerializeField] private float playerRotateSpeed; // how fast the character rotates as it turns
+    [SerializeField] private float playerGroundMoveAcceleration; // how fast the player accelerates from the start of input
+    [SerializeField] private float playerGroundMoveDecceleration; // how fast the player deccelerates after inputs end
+    [SerializeField] private float PLAYER_MAX_SPEED; // the max speed the player can reach
 
-    [SerializeField] private Transform feetPosition;
-    [SerializeField] private Rigidbody rb;
-    [SerializeField] private PlayerInput playerControls;
-    [SerializeField] private InputActionReference playerMovement;
+    [Header("Grounded Movement Components")]
+    private Vector2 playerGroundMoveVelocity; // the directional velocity that the player travels in
 
-    private float gravity;
-    private RaycastHit groundCheck;
-    private Vector2 moveDirection;
+    [Header("Other Variables")]
+    [SerializeField] private float playerMass; // the mass of the player
+    private float gravity; // the gravity force value
+
+    [Header("Other Components")]
+    private Quaternion playerDirection; // the rotational value of the character model in respect to its forward facing vector
+    [SerializeField] private Transform feetPosition; // the position where the floor checks raycasts  
+    [SerializeField] private Rigidbody rb; // the kinematic rigidbody that holds all the physics components
+    [SerializeField] private InputActionReference playerMovement; // the user input references from unity's new input system
+    private RaycastHit groundCheck; // the raycast hit reference that checks for floor collisions
+    private Vector2 moveDirection; // the movement vector that gets it's values based on user input
 
     private void Awake()
     {
         rb = gameObject.GetComponent<Rigidbody>();
         gravity = -9.8f * playerMass;
         feetPosition = transform.GetChild(0).GetComponent<Transform>();
-        playerControls = gameObject.GetComponent<PlayerInput>();
-        playerGroundMoveSpeed = playerGroundMoveVelocity;
     }
 
     void Start()
@@ -39,53 +40,56 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     void Update()
     {
-        moveDirection = playerMovement.action.ReadValue<Vector2>();
+        PlayerMovementRotation(moveDirection); // takes the moveDirection and updates the playerDirection
+
+        moveDirection = playerMovement.action.ReadValue<Vector2>(); // moveDirections vector2 values are read from the playerMovement input map
         transform.rotation = Quaternion.Slerp(transform.rotation, playerDirection, playerRotateSpeed * Time.deltaTime);
-
-        PlayerMovementRotation(moveDirection);
-
-        Debug.Log(playerDirection);
-
+        // ^ the characters rotations is a Quaternion slerp that starts at its current rotation and interpolates to a new rotation whenever the playerDirection is updated
     }
 
     private void FixedUpdate()
     {
 
-        if (moveDirection.y != 0 || moveDirection.x != 0)
+        if (moveDirection.y != 0 || moveDirection.x != 0) // if either of the move inputs are pressed (vertical or horizontal)
         {
-            playerGroundMoveSpeed.x += playerGroundMoveAcceleration * Time.deltaTime * moveDirection.x;
-            playerGroundMoveSpeed.y += playerGroundMoveAcceleration * Time.deltaTime * moveDirection.y;
+            playerGroundMoveVelocity.x += playerGroundMoveAcceleration * Time.deltaTime * moveDirection.x; 
+            playerGroundMoveVelocity.y += playerGroundMoveAcceleration * Time.deltaTime * moveDirection.y;
+            // ^ updates the velocity in respects to acceleration, time and direction for the y and x axis
 
-            if (moveDirection.y != 0 && moveDirection.x != 0)
+            if (moveDirection.y != 0 && moveDirection.x != 0) // if both move inputs are pressed (vertical and horizontal)
             {
-                playerGroundMoveSpeed.x = Mathf.Clamp(playerGroundMoveSpeed.x, -PLAYER_MAX_SPEED / Mathf.Sqrt(2), PLAYER_MAX_SPEED / Mathf.Sqrt(2));
-                playerGroundMoveSpeed.y = Mathf.Clamp(playerGroundMoveSpeed.y, -PLAYER_MAX_SPEED / Mathf.Sqrt(2), PLAYER_MAX_SPEED / Mathf.Sqrt(2));
+                playerGroundMoveVelocity.x = Mathf.Clamp(playerGroundMoveVelocity.x, -PLAYER_MAX_SPEED / Mathf.Sqrt(2), PLAYER_MAX_SPEED / Mathf.Sqrt(2));
+                playerGroundMoveVelocity.y = Mathf.Clamp(playerGroundMoveVelocity.y, -PLAYER_MAX_SPEED / Mathf.Sqrt(2), PLAYER_MAX_SPEED / Mathf.Sqrt(2));
+                // ^ normalizes the x and y velocities when moving to prevent diagnal boosting by dividing the max speed by sqrt(2)
             }
             else
             {
-                playerGroundMoveSpeed.x = Mathf.Clamp(playerGroundMoveSpeed.x, -PLAYER_MAX_SPEED, PLAYER_MAX_SPEED);
-                playerGroundMoveSpeed.y = Mathf.Clamp(playerGroundMoveSpeed.y, -PLAYER_MAX_SPEED, PLAYER_MAX_SPEED);
+                playerGroundMoveVelocity.x = Mathf.Clamp(playerGroundMoveVelocity.x, -PLAYER_MAX_SPEED, PLAYER_MAX_SPEED);
+                playerGroundMoveVelocity.y = Mathf.Clamp(playerGroundMoveVelocity.y, -PLAYER_MAX_SPEED, PLAYER_MAX_SPEED);
+                // ^ sets the max speed to 15 or -15 depending on direction and prevents the velocity from exceeding
             }
         }
         if (moveDirection.x == 0)
         {
-            playerGroundMoveSpeed.x = Mathf.MoveTowards(playerGroundMoveSpeed.x, 0, playerGroundMoveDecceleration * Time.deltaTime);
+            playerGroundMoveVelocity.x = Mathf.MoveTowards(playerGroundMoveVelocity.x, 0, playerGroundMoveDecceleration * Time.deltaTime);
         }
         if (moveDirection.y == 0)
         {
-            playerGroundMoveSpeed.y = Mathf.MoveTowards(playerGroundMoveSpeed.y, 0, playerGroundMoveDecceleration * Time.deltaTime);
+            playerGroundMoveVelocity.y = Mathf.MoveTowards(playerGroundMoveVelocity.y, 0, playerGroundMoveDecceleration * Time.deltaTime);
         }
 
-        if (playerGroundMoveSpeed != Vector2.zero)
-        {
-            rb.MovePosition(new Vector3(transform.position.x + playerGroundMoveSpeed.x,
-                                        transform.position.y,
-                                        transform.position.z + playerGroundMoveSpeed.y));
-        }
 
-        if (!IsGrounded())
+        rb.MovePosition(new Vector3(transform.position.x + playerGroundMoveVelocity.x,
+                                    transform.position.y + (gravity * Time.fixedDeltaTime),
+                                    transform.position.z + playerGroundMoveVelocity.y));
+
+        if (IsGrounded())
         {
-            rb.MovePosition(new Vector3(transform.position.x, transform.position.y + gravity * Time.deltaTime, transform.position.z));
+            gravity = 0;
+        }
+        else
+        {
+            gravity = -9.8f;
         }
 
         Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward), Color.yellow);
@@ -96,7 +100,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
         if (Physics.Raycast(feetPosition.position, transform.TransformDirection(Vector3.down), out groundCheck, Mathf.Infinity))
         {
             Debug.DrawRay(feetPosition.position, transform.TransformDirection(Vector3.down * groundCheck.distance), Color.green);
-            return groundCheck.distance < .2f;
+            return groundCheck.distance < .3f;
         }
         return false;
     }
